eb524883df4e9ca630da5e0e1b5a8fe2c7a3a5fb
Jeffesson Gomes <jeffesson01@gmail.com>
2020-06-07 17:37:52 -0300

    :art: Criando validacao de dados do recipient e hash da senha do usuario

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,7 +1,21 @@
+import * as Yup from 'yup';
 import Recipient from '../models/Recipient';
 
 class RecipientController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      complement: Yup.string(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      postal_code: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Bad request' });
+    }
+
     const {
       id,
       name,
@@ -9,7 +23,7 @@ class RecipientController {
       complement,
       state,
       city,
-      postalCode,
+      postal_code,
     } = await Recipient.create(req.body);
 
     return res.json({
@@ -19,7 +33,7 @@ class RecipientController {
       complement,
       state,
       city,
-      postalCode,
+      postal_code,
     });
   }
 }
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -15,6 +15,14 @@ class User extends Model {
         sequelize,
       }
     );
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+
+      return this;
+    });
   }
 
   checkPassword(password) {
