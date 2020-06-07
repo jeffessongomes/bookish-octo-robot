025be5d1403f84795a2401c4ee7eccbf849196fa
Jeffesson Gomes <jeffesson01@gmail.com>
2020-06-07 17:38:38 -0300

    :bug: Consertando seeders

--- src/database/seeds/20200607192007-admin-user.js
+++ src/database/seeds/20200607192007-admin-user.js
@@ -8,7 +8,7 @@ module.exports = {
         {
           name: 'Distribuidora FastFeet',
           email: 'admin@fastfeet.com',
-          password_hash: bcrypt.hashSync('123456', 8),
+          password_hash: '123456',
           created_at: new Date(),
           updated_at: new Date(),
         },
